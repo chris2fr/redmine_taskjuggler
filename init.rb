25c04ef1a6bcaa003a22687374aa1eea3dbc3a09
Tekkub <christopher@mann.fr>
2009-09-03 14:01:27 +0200

    Changement de chemin

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_tj_status do
-  name 'Redmine Tj Status plugin'
+Redmine::Plugin.register :redmine_taskjuggler do
+  name 'Redmine Taskjuggler plugin'
   author 'Chris Mann'
   description 'This plug exports project status into TaskJuggler and will import the dates too !'
-  version '0.0.1'
+  version '0.0.2'
   #menu :application_menu, :tjstatus, { :controller => 'tjstatus', :action => 'index' }, :caption => 'Task Juggler'
   permission :tjstatus, {:tjstatus => [:index, :export, :initial_export]}, :public => true
   menu :project_menu, :tjstatus, { :controller => 'tjstatus', :action => 'test' }, :caption => 'Task Juggler File', :after => :activity, :param => :project_identifier
