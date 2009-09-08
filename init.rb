f7e054c353607a659005791bf57521a902483594
Christopher Mann <christopher@mann.fr>
2009-09-08 15:26:43 +0200

    Modifications pour saisi par développeurs

--- init.rb
+++ init.rb
@@ -5,7 +5,10 @@ Redmine::Plugin.register :redmine_taskjuggler do
   author 'Chris Mann'
   description 'This plug exports project status into TaskJuggler and will import the dates too !'
   version '0.0.2'
+  #permission :taskjuggler, {:taskjuggler => [:index, :export, :initial_export, timetable]}, :public => true
+  #menu :project_menu, :taskjuggler, { :controller => 'taskjuggler', :action => 'test' }, :caption => 'Task Juggler File', :after => :activity, :param => :project_identifier
   #menu :application_menu, :tjstatus, { :controller => 'tjstatus', :action => 'index' }, :caption => 'Task Juggler'
-  permission :taskjuggler, {:taskjuggler => [:index, :export, :initial_export]}, :public => true
-  menu :project_menu, :taskjuggler, { :controller => 'taskjuggler', :action => 'test' }, :caption => 'Task Juggler File', :after => :activity, :param => :project_identifier
+  menu	:top_menu, :taskjuggler, { :controller => 'taskjuggler', :action => 'timetable' }, :caption => 'Workload (Charge de travail)'
+
+
 end
