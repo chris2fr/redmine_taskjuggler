6b9128266a28cf33879f52f0384206f7ab69ba34
Christopher Mann <christopher@mann.fr>
2009-09-10 00:14:56 +0200

    Added language support and arrows

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_taskjuggler do
   #permission :taskjuggler, {:taskjuggler => [:index, :export, :initial_export, timetable]}, :public => true
   #menu :project_menu, :taskjuggler, { :controller => 'taskjuggler', :action => 'test' }, :caption => 'Task Juggler File', :after => :activity, :param => :project_identifier
   #menu :application_menu, :tjstatus, { :controller => 'tjstatus', :action => 'index' }, :caption => 'Task Juggler'
-  menu	:top_menu, :taskjuggler, { :controller => 'taskjuggler', :action => 'timetable' }, :caption => "_CRAW_ (Compte-Rendu d'Activité Wonderbox)"
+  menu	:top_menu, :taskjuggler, { :controller => 'taskjuggler', :action => 'timetable' }, :caption => :workload_label
 
 
 end
