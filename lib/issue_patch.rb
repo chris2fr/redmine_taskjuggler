68583bb3498e7b4522293b49f533e3919c3e558c
Christopher MANN <christopher@mann.fr>
2013-08-06 21:43:00 +0200

    Caught silly mistake & bettered README

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -10,7 +10,7 @@ module IssuePatch
     # Same as typing in the class
     base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
-      has_and_belongs_to_many :user
+      has_and_belongs_to_many :users # forgot the "s"
     end
  
   end
