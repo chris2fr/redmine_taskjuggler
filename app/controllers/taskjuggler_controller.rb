447d34a2aa41ebdb3c0948d57dda294d10ed1e84
Christopher MANN <christopher@mann.fr>
2013-08-09 12:10:23 +0200

    Adding new notes

--- app/controllers/taskjuggler_controller.rb
+++ app/controllers/taskjuggler_controller.rb
@@ -1,5 +1,5 @@
 class TaskjugglerController < ApplicationController
-unloadable
+  unloadable
 
   def export
   end
@@ -9,34 +9,24 @@ unloadable
   end
 
   def index
-	@projects = Project.find(:all, :conditions => ["status = 1 AND parent_id IS NOT NULL"], :order => ["parent_id, name"] )
-	@users = User.find(:all)
+    @projects = Project.find(:all, :conditions => ["status = 1 AND parent_id IS NOT NULL"], :order => ["parent_id, name"] )
+    @users = User.find(:all)
   end
 
-  def initial_export
-
-  end
-
-
-
-      def PutIssuesByCat (issues)
-	    retvar = {}
-	    issues.each do |issue|
-		  if issue.category and issue.category.name
-			cat_name = issue.category.name
-		  else
-			cat_name = "no_category"
-		  end
-		  if not retvar[cat_name]
-			retvar[cat_name] = []
-		  end
-		  retvar[cat_name].push(issue)
-	    end
-	    return retvar
+  def PutIssuesByCat (issues)
+    retvar = {}
+    issues.each do |issue|
+      if issue.category and issue.category.name
+        cat_name = issue.category.name
+      else
+        cat_name = "no_category"
       end
-
-  def test
-	
+      if not retvar[cat_name]
+        retvar[cat_name] = []
+      end
+      retvar[cat_name].push(issue)
+    end
+    return retvar
   end
 
 end
