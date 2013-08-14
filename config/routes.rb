620c62601ddb38ee755bcf88fbdaeb10aaea969b
Christopher Mann <christopher@mann.fr>
2013-08-14 13:58:25 +0200

    Routes error corrected.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-get '/tj3project/:project_identifier', to: 'redmine_taskjuggler#initial_export'
-get '/hello', to: 'redmine_taskjuggler#hello'
-get '/tjadmin', to: 'redmine_taskjuggler#tjadmin'
+#get '/redmine_taskjuggler_project/view/:id', to: 'RedmineTaskjugglerProject#view'
+
+resources :redmine_taskjuggler_projects
+resources :redmine_taskjuggler
