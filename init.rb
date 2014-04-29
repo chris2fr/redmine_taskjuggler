b2329d9b6bf574f7f2b903f3d2a36db09308b882
Christopher Mann <christopher@mann.fr>
2014-04-29 12:51:56 +0200

    Added documentation for taskjuggler.mann.fr

--- init.rb
+++ init.rb
@@ -1,4 +1,10 @@
-# encoding: utf-8
+# encoding:utf-8:noai:expandtab:ts=2:sw=2
+##
+# RedmineTaskjuggler (c) Christopher Mann et al. 2009 - 2014
+# Licence GPL v3.0 Affero
+# https://github.com/chris2fr/redmine_taskjuggler/
+# File : init.rb
+# Code for integrating redmine_taskjuggler into Redmine
 
 ##
 # Patches to the Redmine core.
