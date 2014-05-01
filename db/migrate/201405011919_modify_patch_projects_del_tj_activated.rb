96084a086bb08e1c5e7c3bc3ce0abe639ce32735
Christopher Mann <christopher@mann.fr>
2014-05-01 19:32:40 +0200

    Typo in the migration script

--- db/migrate/201405011919_modify_patch_projects_del_tj_activated.rb
+++ db/migrate/201405011919_modify_patch_projects_del_tj_activated.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 ##
 # Activated is now handled by the activation of the module or not
-class ModifyPatchProjectDelTjActivated < ActiveRecord::Migration
+class ModifyPatchProjectsDelTjActivated < ActiveRecord::Migration
   def change
     remove_column :projects, :tj_activated # Add arbitrary data to an issu
   end
